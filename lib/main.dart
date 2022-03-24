5916455e92820785719a8193d521b0b21c09dd14
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-24 11:09:39 +0300

    add button row

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
 
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
@@ -54,6 +64,7 @@ class MyApp extends StatelessWidget {
           body: Column(
             children: [
               titleSection,
+              buttonSection,
             ],
           ),
         ),
@@ -65,7 +76,18 @@ class MyApp extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
       ],
     );
   }
